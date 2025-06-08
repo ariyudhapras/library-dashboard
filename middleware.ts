961cd3b4ce739a70e67f95c942c3b555cc4d539a
ariyudhapras <ariyudhapras@gmail.com>
2025-06-08 20:10:45 +0700

    Update dynamic export declaration in authentication route and refine middleware matcher; enhance routing behavior and maintainability.

--- middleware.ts
+++ middleware.ts
@@ -116,6 +116,6 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!api/auth|_next/static|_next/image|favicon.ico).*)",
   ],
 };
